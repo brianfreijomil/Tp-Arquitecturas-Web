60a5c30ffd07e17ce30113f9959d421bb396a449
Brian Freijomil <brianfreijomil@hotmail.com>
2023-08-25 15:20:26 -0300

    nada

--- practico1/src/main/java/org/example/Main.java
+++ practico1/src/main/java/org/example/Main.java
@@ -7,10 +7,9 @@ import java.sql.SQLException;
 public class Main {
 
     public static DaoPostgres daoPostgres = new DaoPostgres();
-
     public static void main(String[] args) throws SQLException {
         daoPostgres.startConnection();
-        System.out.println("Lista de personas:");
+        System.out.println("Lista de personas: ");
         for (Persona p : daoPostgres.getPersonas()) {
             System.out.println(p.toString());
         }
