9f57dd159575000b0c6f74d0fc878b779b689053
Brian Freijomil <brianfreijomil@hotmail.com>
2023-10-03 10:11:00 -0300

    nuevo proyecto

---
+++ Integrador3/src/main/java/com/arquitecturasWeb/Integrador3/entities/Estudiante.java
@@ -0,0 +1,97 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+public class Estudiante {
+
+    @Id
+    private int DNI;
+    @Column
+    private int nro_libreta;
+    @Column(nullable = false)
+    private String apellido;
+    @Column(nullable = false)
+    private String nombre;
+    @Column
+    private int edad;
+    @Column(nullable = true)
+    private String genero;
+    @Column(nullable = false)
+    private String ciudad;
+
+    public Estudiante() {
+        super();
+    }
+
+    public Estudiante(int DNI, String nombre, String apellido, int edad,String genero, int nro_libreta, String ciudad) {
+        super();
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.genero = genero;
+        this.nro_libreta = nro_libreta;
+        this.ciudad = ciudad;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getNro_libreta() {
+        return nro_libreta;
+    }
+
+    public void setNro_libreta(int nro_libreta) {
+        this.nro_libreta = nro_libreta;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante: "+DNI+", "+apellido+", "+nombre+". "+ciudad;
+    }
+}
