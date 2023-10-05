ba55ed8fe1ee82e9a7a03b76b00a9c4b67db2501
Brian Freijomil <brianfreijomil@hotmail.com>
2023-10-05 16:36:22 -0300

    se acomodo controller y service de studentcareer

--- Integrador3/src/main/java/com/arquitecturasWeb/Integrador3/service/StudentCareerService.java
+++ Integrador3/src/main/java/com/arquitecturasWeb/Integrador3/service/StudentCareerService.java
@@ -31,5 +31,11 @@ public class StudentCareerService {
         List<StudentsOfCareerByCityResponseDTO> students = repository.findStudentByCareerAndCity();
         return students;
     }
+    @Transactional(readOnly = true)
+    public List<ReportCareerDTO> reportOfCareers() {
+        return this.repository.findInscriptionAndGraduatedForYear().stream()
+                .map(result -> new ReportCareerDTO((String) result[0], ((BigDecimal) result[1]), ((BigDecimal) result[2]), ((Long) result[3])))
+                .collect(Collectors.toList());
+    }
 
 }
